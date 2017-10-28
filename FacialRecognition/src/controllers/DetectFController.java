9b4b1b2b7eb41846edd632a5550e8199e00a3e5a
olgakup <olga.kupchevskaya.812@my.csun.edu>
2017-10-28 15:43:27 -0700

    push

--- FacialRecognition/src/controllers/DetectFController.java
+++ FacialRecognition/src/controllers/DetectFController.java
@@ -6,18 +6,15 @@ import java.io.IOException;
 
 import javax.imageio.ImageIO;
 
-import org.opencv.core.Core;
 import org.opencv.core.Mat;
 import org.opencv.core.MatOfRect;
 import org.opencv.core.Point;
 import org.opencv.core.Rect;
 import org.opencv.core.Scalar;
-import org.opencv.core.Size;
-import org.opencv.highgui.Highgui;
 import org.opencv.imgcodecs.Imgcodecs;
 import org.opencv.imgproc.Imgproc;
 import org.opencv.objdetect.CascadeClassifier;
-import org.opencv.objdetect.Objdetect;
+
 
 
 
